4187f2eadef25990401c1a126b08440c21921862
Olabimpe <abdulbimpe@gmail.com>
2022-09-12 14:02:31 -0700

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,9 @@ int main(void)
 { char a;
 
 for (a = 'a'; a <= 'z' ; a++)
-{ if (a == 'q' || a == 'e')
+{
+if (a == 'q' || a == 'e')
 { continue; }
 putchar(a); }
 putchar('\n');
-return (0);
-}
+return (0); }
