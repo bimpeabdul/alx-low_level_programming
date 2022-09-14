83fdc444222349e81b86e7d5a1798c9f018b6b95
Olabimpe <abdulbimpe@gmail.com>
2022-09-14 13:03:46 -0700

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,8 @@ void print_alphabet_x10(void)
 { char r;
 int a = 0;
 
-while  (a < 10)
-
-{ 
-for (r = 'a'; r <= 'z'; r++)
-{ _putchar(r); }
+while (a < 10)
+{ for (r = 'a'; r <= 'z'; r++)
+{_putchar(r); }
+_putchar('\n');				
 a++; } }
