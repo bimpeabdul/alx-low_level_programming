cb1376ebcffa2bfc1c49e381cea092fe7468de81
Olabimpe <abdulbimpe@gmail.com>
2022-09-14 10:35:48 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,18 @@
-#include "main.h"                                                                                                                       
-/**                                                                                                                                     
-* main - Entry point                                                                                                                    
-* 
-* Return: always 0(Success)                                                                                                             
-*/                                                                                                                                      
-int main(void)                                                                                                                          
-{ char str[] = "_putchar";                                                                                                              
-int ch;                                                                                                                                 
+#include "main.h"
 
-for (ch = 0; ch < 8; ++ch)                                                                                                              
-_putchar(str[ch]);                                                                                                                      _putchar('\n');
-
+/**
+* main - Entry point
+*
+* Return: Always returns 0
+*/
+int main(void)
+{ _putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0); }
